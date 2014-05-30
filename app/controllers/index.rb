46650ef3914a00c6f90bca71c95f7e5b75ee9f82
William Tsay <willtsay@WTSAY.local>
2014-05-29 19:26:38 -0700

    WOOOOOOOGOINGCRAZY

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,27 +1,67 @@
+require 'httparty'
+
+
+CLIENT_ID = "494745400143-ggt69ru9s24joh3ihik4psu91o74nlra.apps.googleusercontent.com"
+CLIENT_SECRET = "7-8xV3iGpxeFnio-mEX7Y6B4"
+STATE = SecureRandom.hex
+
+
 get '/' do
-  # Look in app/views/index.erb
   erb :index
 end
 
-get '/bands' do
-  @band_names = Band.all.map(&:name)
-  erb :bands
+get '/google_login' do
+  session[:id] = nil
+  google_url = "https://accounts.google.com/o/oauth2/auth?" +
+                    "response_type=code&"+
+                    "client_id=#{CLIENT_ID}&"+
+                    "redirect_uri=http://hidden-reaches-5049.herokuapp.com/logged_in&"+
+                    "scope=email%20profile&"+
+                    "state=#{STATE}&" 
+  redirect google_url
 end
 
-post '/bands' do
-  new_band = Band.create!(name: params[:name])
-  redirect "/bands/#{new_band.id}"
+get '/logged_in' do
+  access_code = params[:code]
+  first_response = HTTParty.post("https://accounts.google.com/o/oauth2/token?", {body: {client_id: CLIENT_ID, client_secret: CLIENT_SECRET, redirect_uri: "http://hidden-reaches-5049.herokuapp.com/logged_in", grant_type:"authorization_code", code: access_code}})
+  access_token = first_response["access_token"]
+  user_info = HTTParty.get("https://www.googleapis.com/plus/v1/people/me?", {query: {access_token: access_token}})
+  #check if their email is in the database, otherwise write them in. 
+  email = user_info["emails"][0]["value"]
+  if User.find_by_email(email).any?
+    session[:id] = User.find_by_email(email).id
+  else
+    user = User.create(email: email)
+    session[:id] = user.id
+  end
+  redirect '/'
 end
 
-get '/bands/new' do
-  erb :new_band
+get '/log_out' do
+  #handle these with ajax requests??
+  session[:id] = nil
 end
 
-get '/bands/:id' do
-  @band = Band.find(params[:id])
-  erb :show_band
+get '/highscores' do
+  users = User.all
+  scores = users.map do |user|
+    user.highscore
+  end
+  scores.sort!
+  scores.reverse!
+  @scores = scores.take(10)
+  #sort the users by the highest of scores/ get the top 10. 
+  erb :highscores
 end
 
-get '/info' do
-  Demo.new(self).info
+post '/highscores' do
+  if session[:id]
+    user = User.find(session[:id])
+    if user.highscore < params[:highscore]
+      user.highscore = params[:highscore]
+    end
+    user.highscore
+  end
 end
+
+
